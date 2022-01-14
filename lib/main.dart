c844bd176115cbbe82c54fbdcec9230e01f3dd59
GAUTAM <gautam@gmail.com>
2022-01-14 22:18:51 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Material(
+        child: Container(color: Colors.white,
+          child: Center(child: Text("GAUTAM"),),),
+      ),
+    );
+  }
+}
