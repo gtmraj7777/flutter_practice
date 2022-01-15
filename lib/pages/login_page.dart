63dd039914d5d543f7153c119cddfb4a21ecbb2b
GAUTAM <gautam@gmail.com>
2022-01-15 16:04:06 +0530

    Day5 complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:first_app11/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -7,41 +8,43 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        children: [
-        Image.asset("assets/images/login_image.jpg",fit: BoxFit.cover,),
-        SizedBox(
-          height: 20,
-        ),
-        Text("Welcome",style: TextStyle(fontSize: 30.0,fontFamily: GoogleFonts.lateef().fontFamily),),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_image.jpg",fit: BoxFit.cover,),
+            SizedBox(
+              height: 20,
+            ),
+            Text("Welcome",style: TextStyle(fontSize: 30.0,fontFamily: GoogleFonts.lateef().fontFamily),),
 
-          Padding(padding: EdgeInsets.all(10.0),
-          child: Column(
-            children: [
+            Padding(padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: [
 
-              TextFormField(decoration: InputDecoration(
-                hintText: "UserName",
-                labelText: "UserName",
-              ),),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                hintText: "Enter Password",
-                labelText: "Password",
-              ),),
-              SizedBox(
-                height: 20,
+                  TextFormField(decoration: InputDecoration(
+                    hintText: "UserName",
+                    labelText: "UserName",
+                  ),),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(onPressed: () {
+                    Navigator.pushNamed(context, MyRoutes.homeRoute);
+
+                  }, child: Text("Click"),style: TextButton.styleFrom(minimumSize: Size(120, 50)),)
+
+                ],
               ),
-              ElevatedButton(onPressed: () {
-                print("GAUTAM");
+            ),
 
-              }, child: Text("Click"))
-              
-            ],
-          ),
-          ),
-
-        ],
+          ],
+        ),
       ),
     );
   }
