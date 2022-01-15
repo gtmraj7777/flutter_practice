8896917c59f484b2ea9e770f189a2cafbf740346
GAUTAM <gautam@gmail.com>
2022-01-15 18:02:51 +0530

    day6 complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,8 +2,16 @@ import 'package:first_app11/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = '';
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +23,7 @@ class LoginPage extends StatelessWidget {
             SizedBox(
               height: 20,
             ),
-            Text("Welcome",style: TextStyle(fontSize: 30.0,fontFamily: GoogleFonts.lateef().fontFamily),),
+            Text("Welcome $name",style: TextStyle(fontSize: 30.0,fontFamily: GoogleFonts.lateef().fontFamily),),
 
             Padding(padding: EdgeInsets.all(10.0),
               child: Column(
@@ -24,7 +32,10 @@ class LoginPage extends StatelessWidget {
                   TextFormField(decoration: InputDecoration(
                     hintText: "UserName",
                     labelText: "UserName",
-                  ),),
+                  ),onChanged: (value) {
+                    name = value;
+                  setState(() {});
+                  },),
                   TextFormField(
                     obscureText: true,
                     decoration: InputDecoration(
@@ -34,10 +45,30 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 20,
                   ),
-                  ElevatedButton(onPressed: () {
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
 
-                  }, child: Text("Click"),style: TextButton.styleFrom(minimumSize: Size(120, 50)),)
+                  InkWell(
+                    onTap: () async{
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: AnimatedContainer(
+                      height: 50,
+                      width: changeButton? 60: 120,
+                      alignment: Alignment.center,
+                      //color: Colors.deepPurpleAccent,
+                      child: changeButton?Icon(Icons.done,color: Colors.white,):Text("Click",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
+                      decoration: BoxDecoration(color: Colors.blue,
+
+                          borderRadius: BorderRadius.circular(changeButton ? 20 :10)), duration: Duration(seconds: 1),
+                    ),
+                  ),
+                  // ElevatedButton(onPressed: () {
+                  //   Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //
+                  // }, child: Text("Click"),style: TextButton.styleFrom(minimumSize: Size(120, 50)),),
 
                 ],
               ),
