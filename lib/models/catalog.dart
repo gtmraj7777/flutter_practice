b67e53e8a021da641c7713507d3cef23cea3036a
GAUTAM <gautam@gmail.com>
2022-01-16 14:41:52 +0530

    Day10 complete

---
+++ lib/models/catalog.dart
@@ -0,0 +1,22 @@
+
+
+class Item
+{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+
+
+
+}
+
+
+final products = [
+  Item(id: "Ranchi", name: "Iphone", desc: "Apple 12 pro", price: 999, color: "#33505a", image: "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2021/12/14/1009949-virat-kohli.jpg")
+
+];
