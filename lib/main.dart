aa1fee2446cdbfc9caf662633b353441c1f03e54
GAUTAM <gautam@gmail.com>
2022-01-16 14:27:22 +0530

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_app11/pages/home_page.dart';
 import 'package:first_app11/pages/login_page.dart';
 import 'package:first_app11/utils/routes.dart';
+import 'package:first_app11/widgets/themes.dart';
 import 'package:flutter/material.dart';
 
 
@@ -20,6 +21,8 @@ class MyWidget extends StatelessWidget {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+    theme: MyTheme.lightTheme(context),
+     darkTheme: MyTheme.darkTheme(context),
      // home:HomePage(),
       initialRoute: MyRoutes.homeRoute,
       routes: {
