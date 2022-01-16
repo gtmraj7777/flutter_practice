90e1b9deef62d17bbf5a206e81f8f833551fc927
GAUTAM <gautam@gmail.com>
2022-01-16 17:10:03 +0530

    Decode json complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,14 +2,36 @@ import 'package:first_app11/models/catalog.dart';
 import 'package:first_app11/widgets/drawer.dart';
 import 'package:first_app11/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   //const HomePage({Key? key}) : super(key: key);
   final double day = 100;
+
   final dummyList = List.generate(20, (index) => CatalogModals.items[0]);
 
-  // HomePage({Key? key}) : super(key: key);
+    @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
 
+    loadData();
+  }
+
+  void loadData() async{
+  var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+  // print(catalogJson);
+   var decodeData = jsonDecode(catalogJson);
+   print(decodeData["products"]);
+  }
+
+  // HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,4 +47,6 @@ class HomePage extends StatelessWidget {
       drawer: const MyDrawer(),
     );
   }
+
+
 }
